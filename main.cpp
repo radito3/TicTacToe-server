a6e1a82fddd750b01afa9ca2f364479e7faabc76
Rangel Ivanov <rangel.ivanov@sap.com>
2021-05-09 20:53:11 +0300

    Update socket connection handling
    - Change server port to standard http port 80
    - Handle errors from system calls
    - Use `send` in a loop in case not all of the bytes transmit in one call

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <sstream>
 #include <map>
+#include <string_view>
 
 #include <string.h>
+#include <errno.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
+#define check_err(func) if (errno != 0) \
+                            perror(#func);
 
 class http_request {
     std::string_view http_method;
@@ -27,7 +32,7 @@ class http_response {
 
 public:
 
-    const char* to_c_str() const {
+    char* to_c_str() const {
         return "";
     }
 };
@@ -42,9 +47,11 @@ http_response handle_request(const http_request& request) {
     return http_response();
 }
 
+//TODO configure signal handles for SIGTERM, SIGKILL (and maybe others) to close the open connections & call appropriate dtors
+
 int main() {
-    int sockfd = 0, connfd = 0;
-    struct sockaddr_in serveraddr, connaddr;
+    int sockfd = 0;
+    struct sockaddr_in serveraddr;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
@@ -56,7 +63,7 @@ int main() {
 
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serveraddr.sin_port = htons(8080);
+    serveraddr.sin_port = htons(80);
 
     if (bind(sockfd, (struct sockaddr*) &serveraddr, sizeof(serveraddr)) != 0) {
         perror("bind");
@@ -69,7 +76,25 @@ int main() {
     }
 
     while (true) {
-        connfd = accept(sockfd, (struct sockaddr*) &connaddr, NULL);
+        struct sockaddr_in connaddr;
+        int connfd = accept(sockfd, (struct sockaddr*) &connaddr, NULL);
+        
+        //is this really necessary?
+        switch (connfd) {
+            case EINTR:
+            case ECONNABORTED:
+                continue;
+            case ENOBUFS:
+            case ENOMEM:
+                close(sockfd);
+                check_err(close);
+                return -1;
+            case EPERM:
+                perror("accept");
+                return -1;
+            default:
+                break;
+        }
 
         //these will be used for creating the gRPC stubs
         char *connection_address = inet_ntoa(connaddr.sin_addr);
@@ -77,7 +102,7 @@ int main() {
 
         std::stringstream packets;
         ssize_t bytes_received = -1;
-        char packet[1024];
+        char packet[1024]; //TODO make the packet size configurable
         memset(packet, '0', sizeof(packet));
 
         //recv
@@ -90,13 +115,24 @@ int main() {
             }
             packets << packet;
         }
+        check_err(recv);
+        
         http_request request = parse_http_request(packets);
         http_response response = handle_request(request);
-        const char* response_str = response.to_c_str();
+        char* response_str = response.to_c_str();
+        size_t response_size = strlen(response_str);
 
-        send(connfd, (const void *) response_str, strlen(response_str), 0);
+        ssize_t bytes_sent = -1;
+        while ((bytes_sent = send(connfd, (void *) response_str, response_size, 0)) > 0) {
+            if (bytes_sent < response_size) {
+                response_str += bytes_sent;
+                response_size -= bytes_sent;
+            }
+        }
+        check_err(send);
 
         close(connfd);
+        check_err(close);
     }
 
     if (close(sockfd) != 0) {
